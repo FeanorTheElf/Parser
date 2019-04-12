fea6addee4217fca270af4e3430c31c519282e12
Simon Pohmann <simon@pohmann.de>
2019-04-12 14:31:06 +0200

    Extracted parser generator to its own file

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,26 @@
 #![allow(warnings)]
-//#![feature(trace_macros)]
-
-//trace_macros!(true);
 
 mod tokens;
 mod ast;
 mod lexer;
-//mod parser;
-//mod rust_backend;
+#[macro_use]
+mod parser_gen;
 
-use std::vec::Vec;
+use parser_gen::Parse;
 use lexer::lex;
-use ast::{BaseType};
-use tokens::{Stream, Token, Keyword};
-
-macro_rules! rule_alt_parser {
-    ($stream:ident;) => {
-		()
-	};
-	($stream:ident; { $name:ident }) => {
-        ({
-			let mut els: Vec<$name> = Vec::new();
-			while $name::guess_can_parse($stream) {
-				els.push($name::parse($stream));
-			}
-			els
-		}, ())
-    };
-	($stream:ident; Token#$token:ident $($tail:tt)*) => {
-        {($stream).expect_next(&Token::$token); rule_alt_parser!($stream; $($tail)*)}
-    };
-    ($stream:ident; $name:ident $($tail:tt)*) => {
-        ($name::parse($stream), rule_alt_parser!($stream; $($tail)*))
-    };
-}
-
-macro_rules! rule_base_alt_parser {
-	($stream:ident; $result:ident; $else_code:tt; $variant:ident(Token#$token:ident $($tail:tt)*)) => {
-        if $stream.ends(&Token::$token) {
-			$result::$variant(rule_alt_parser!($stream; Token#$token $($tail)*))
-		} else {
-			$else_code
-		}
-    };
-    ($stream:ident; $result:ident; $else_code:tt; $variant:ident($name:ident $($tail:tt)*)) => {
-        if $name::guess_can_parse($stream) {
-			$result::$variant(rule_alt_parser!($stream; $name $($tail)*))
-		} else {
-			$else_code
-		}
-    };
-}
-
-macro_rules! rule_parser {
-	($stream:ident; $result:ident; $variant:ident $alt:tt) => {
-        rule_base_alt_parser!($stream; $result; {
-			panic!("Unexpected tokens: {:?}", $stream)
-		}; $variant $alt)
-    };
-    ($stream:ident; $result:ident; $variant:ident $alt:tt | $($tail:tt)*) => {
-		rule_base_alt_parser!($stream; $result; {
-			rule_parser!($stream; $result; $($tail)*)
-		}; $variant $alt)
-    };
-}
-
-macro_rules! rule_alt_guess_can_parse {
-	($stream:ident; $variant:ident(Token#$token:ident $($tail:tt)*)) => {
-        ($stream).ends(&Token::$token)
-    };
-    ($stream:ident; $variant:ident($name:ident $($tail:tt)*)) => {
-        $name::guess_can_parse($stream)
-    };
-}
-
-macro_rules! rule_guess_can_parse {
-	($stream:ident; $variant:ident $alt:tt) => {
-        rule_alt_guess_can_parse!($stream; $variant $alt)
-    };
-    ($stream:ident; $variant:ident $alt:tt | $($tail:tt)*) => {
-		rule_alt_guess_can_parse!($stream; $variant $alt) || rule_guess_can_parse!($stream; $($tail)*)
-    };
-}
-
-macro_rules! impl_parse {
-	($result:ident -> $($tail:tt)*) => {
-		impl Parse for $result {
-			fn guess_can_parse(stream: &Stream) -> bool {
-				rule_guess_can_parse!(stream; $($tail)*)
-			}
-			fn parse(stream: &mut Stream) -> Self {
-				rule_parser!(stream; $result; $($tail)*)
-			}
-		}
-	}
-}
+use tokens::{Identifier, Literal, Keyword, Token, Stream};
+use ast::{Func, Stmts, Stmt, Type, Expr, Summand, ExprMult, ProductPart, ExprUn, BasicExprUn, ParamDeclaration, BaseType};
 
 #[derive(Debug)]
 enum Test {
 	Var1((BaseType, (BaseType, ()))),
-	Var2((Vec<Foo>, ()))
+	Var2((Vec<Foo>, ())),
+	Var3((Foo, (Vec<BaseType>, ())))
 }
 
-impl_parse!(Test -> Var1(BaseType Token#OpAdd BaseType)|Var2(Token#OpMult {Foo}));
-
-trait Parse {
-	fn guess_can_parse(stream: &Stream) -> bool;
-	fn parse(stream: &mut Stream) -> Self;
-}
+impl_parse!{ Test -> Var1(BaseType Token#OpAdd BaseType) 
+                   | Var2(Token#OpMult {Foo})
+				   | Var3(Token#Colon Foo Token#OpSmallerEq {BaseType}) }
 
 #[derive(Debug)]
 enum Foo {
@@ -137,6 +50,6 @@ impl Parse for BaseType {
 }
 
 fn main() {
-	let mut stream = lex("int*if if if".to_string());
+	let mut stream = lex("*if if".to_string());
 	println!("{:?}", Test::parse(&mut stream));
 }
---
+++ src/parser_gen.rs
@@ -0,0 +1,89 @@
+use super::tokens::{Stream, Token, Keyword};
+use std::vec::Vec;
+
+pub trait Parse {
+	fn guess_can_parse(stream: &Stream) -> bool;
+	fn parse(stream: &mut Stream) -> Self;
+}
+
+macro_rules! rule_alt_parser {
+    ($stream:ident;) => {
+		()
+	};
+	($stream:ident; { $name:ident }) => {
+        ({
+			let mut els: Vec<$name> = Vec::new();
+			while $name::guess_can_parse($stream) {
+				els.push($name::parse($stream));
+			}
+			els
+		}, ())
+    };
+	($stream:ident; Token#$token:ident $($tail:tt)*) => {
+        {($stream).expect_next(&Token::$token); rule_alt_parser!($stream; $($tail)*)}
+    };
+    ($stream:ident; $name:ident $($tail:tt)*) => {
+        ($name::parse($stream), rule_alt_parser!($stream; $($tail)*))
+    };
+}
+
+macro_rules! rule_base_alt_parser {
+	($stream:ident; $result:ident; $else_code:tt; $variant:ident(Token#$token:ident $($tail:tt)*)) => {
+        if $stream.ends(&Token::$token) {
+			$result::$variant(rule_alt_parser!($stream; Token#$token $($tail)*))
+		} else {
+			$else_code
+		}
+    };
+    ($stream:ident; $result:ident; $else_code:tt; $variant:ident($name:ident $($tail:tt)*)) => {
+        if $name::guess_can_parse($stream) {
+			$result::$variant(rule_alt_parser!($stream; $name $($tail)*))
+		} else {
+			$else_code
+		}
+    };
+}
+
+macro_rules! rule_parser {
+	($stream:ident; $result:ident; $variant:ident $alt:tt) => {
+        rule_base_alt_parser!($stream; $result; {
+			panic!("Unexpected tokens: {:?}", $stream)
+		}; $variant $alt)
+    };
+    ($stream:ident; $result:ident; $variant:ident $alt:tt | $($tail:tt)*) => {
+		rule_base_alt_parser!($stream; $result; {
+			rule_parser!($stream; $result; $($tail)*)
+		}; $variant $alt)
+    };
+}
+
+macro_rules! rule_alt_guess_can_parse {
+	($stream:ident; $variant:ident(Token#$token:ident $($tail:tt)*)) => {
+        ($stream).ends(&Token::$token)
+    };
+    ($stream:ident; $variant:ident($name:ident $($tail:tt)*)) => {
+        $name::guess_can_parse($stream)
+    };
+}
+
+macro_rules! rule_guess_can_parse {
+	($stream:ident; $variant:ident $alt:tt) => {
+        rule_alt_guess_can_parse!($stream; $variant $alt)
+    };
+    ($stream:ident; $variant:ident $alt:tt | $($tail:tt)*) => {
+		rule_alt_guess_can_parse!($stream; $variant $alt) || rule_guess_can_parse!($stream; $($tail)*)
+    };
+}
+
+macro_rules! impl_parse {
+	($result:ident -> $($tail:tt)*) => {
+		impl Parse for $result {
+			fn guess_can_parse(stream: &Stream) -> bool {
+				rule_guess_can_parse!(stream; $($tail)*)
+			}
+			fn parse(stream: &mut Stream) -> Self {
+				rule_parser!(stream; $result; $($tail)*)
+			}
+		}
+	}
+}
