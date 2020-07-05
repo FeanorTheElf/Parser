2785830dbf3e968e914cc994cfcb9739d0406f32
Simon Pohmann <simon@pohmann.de>
2020-07-05 17:49:38 +0200

    Add test and fix

--- src/cuda/expression.rs
+++ src/cuda/expression.rs
@@ -4,7 +4,15 @@ use super::declaration::*;
 use super::writer::*;
 
 pub trait CudaWritableExpression {
+    /// Writes code in the target language to out that evaluates this expression and can
+    /// be inserted as a parameter in a function call.
     fn write_parameter_context(&self, param_type: &Type, out: &mut CodeWriter) -> Result<(), OutputError>;
+    /// Writes code in the target language to out that evaluates this expression and can
+    /// be included in a context where the target language requires a value. Some types
+    /// in the source language can only be represented by multiple variables in the target
+    /// language, and therefore expressions yielding these types cannot be written using
+    /// this function.
+    fn write_value_context(&self, out: &mut CodeWriter) -> Result<(), OutputError>;
 }
 
 fn get_level(op: BuiltInIdentifier) -> i32 {
@@ -26,16 +34,20 @@ fn get_level(op: BuiltInIdentifier) -> i32 {
 }
 
 /// Writes to out all operands separated by the symbol associated with the given operator
-fn write_op_expression<I>(op: BuiltInIdentifier, operands: I, out: &mut CodeWriter) -> Result<(), OutputError>
+fn write_operator_expression<I>(op: BuiltInIdentifier, operands: I, out: &mut CodeWriter) -> Result<(), OutputError>
     where I: Iterator, I::Item: FnOnce(&mut CodeWriter) -> Result<(), OutputError>
 {
     out.write_separated(operands, |out| write!(out, " {} ", op.get_symbol()).map_err(OutputError::from))
 }
 
-/// Asserts that the expression is valid in a context that requires a value of a type that is
-/// only representable by multiple variables in the target language. Currently, this are only
-/// variables and not dynamically calculated values.
-fn expect_expression_valid_in_context_complex_type(expr: &Expression) -> Result<&Identifier, OutputError> {
+/// Asserts that the expression is valid in a context that requires a complex type (i.e. a type
+/// that is represented by multiple values in the target language, as e.g. an array).
+/// Currently, this are only variables, as function calls can only return a single value
+/// in the target language. Other return values are therefore returned via output parameter, and
+/// chaining functions with output parameters is both difficult and hard to understand.
+/// 
+/// If this is a variable, the corresponding identifier is returned.
+fn assert_expression_valid_as_complex_type(expr: &Expression) -> Result<&Identifier, OutputError> {
     match expr  {
         Expression::Call(call) => panic!("Expecting the function call at {} to yield a value representable as
             a single value in the target language. The code generator cannot deal with intermediate values that
@@ -49,32 +61,37 @@ fn expect_expression_valid_in_context_complex_type(expr: &Expression) -> Result<
     }
 }
 
+fn write_target_index_calculation<'a, I>(indexed_array_name: &Name, dimension_count: u32, indices: I, out: &mut CodeWriter) -> Result<(), OutputError>
+    where I: Iterator<Item = &'a Expression>
+{
+    out.write_separated(indices.enumerate().map(|(dimension, index)| 
+        move |out: &mut CodeWriter| {
+            // the length in the last dimension is 1
+            if dimension as u32 + 1 < dimension_count {
+                indexed_array_name.write_dim(dimension as u32 + 1, out)?;
+                write!(out, " * ")?;
+            }
+            write_value_context(index, out, get_level(BuiltInIdentifier::FunctionMul))?;
+            Ok(())
+        }), 
+        |out| write!(out, " + ").map_err(OutputError::from))
+}
+
 /// Writes to out the index expression with the given parameters, i.e. the expression for element 
 /// accessing the first operand, where the indices are all the following operands
 fn write_index_expression(op: BuiltInIdentifier, operands: &Vec<Expression>, out: &mut CodeWriter) -> Result<(), OutputError> {
     debug_assert_eq!(BuiltInIdentifier::FunctionIndex, op);
     let mut param_iter = operands.iter();
     let indexed_array = param_iter.next().unwrap();
-    let indexed_array_name = match expect_expression_valid_in_context_complex_type(indexed_array)? {
+    let indexed_array_name = match assert_expression_valid_as_complex_type(indexed_array)? {
         Identifier::Name(name) => name,
         Identifier::BuiltIn(_) => unimplemented!("Indexing builtin identifiers is not implemented")
     };
-    let dimension_count = operands.len() - 1;
-    let indexed_array_name_ref = &indexed_array_name;
     write_value_context(indexed_array, out, get_level(op))?;
     write!(out, "[")?;
-    out.write_separated(param_iter.enumerate().map( |(dimension, index)| move |out: &mut CodeWriter| {
-            // the length in the last dimension is 1
-            if dimension + 1 < dimension_count {
-                indexed_array_name_ref.write_dim(dimension as u32 + 1, out)?;
-                write!(out, " * ")?;
-            }
-            write_value_context(index, out, get_level(BuiltInIdentifier::FunctionMul))?;
-            Ok(())
-        }), 
-        |out| write!(out, " + ").map_err(OutputError::from))?;
+    write_target_index_calculation(indexed_array_name, operands.len() as u32 - 1, param_iter, out)?;
     write!(out, "]")?;
-    Ok(())
+    return Ok(());
 }
 
 fn write_unary_operation_expression(op: BuiltInIdentifier, operand: &Expression, out: &mut CodeWriter) -> Result<(), OutputError> {
@@ -101,7 +118,7 @@ fn write_builtin_function_call_value(op: BuiltInIdentifier, operands: &Vec<Expre
     } else if op == BuiltInIdentifier::FunctionIndex {
         write_index_expression(op, operands, out)?;
     } else {
-        write_op_expression(op, operands.iter().map(|index| move |out: &mut CodeWriter| write_value_context(index, out, current_level)), out)?;
+        write_operator_expression(op, operands.iter().map(|index| move |out: &mut CodeWriter| write_value_context(index, out, current_level)), out)?;
     }
     if current_level <= parent_expr_level {
         write!(out, ")")?;
@@ -162,29 +179,28 @@ fn write_variable_parameter_context(pos: &TextPosition, variable: &Name, param_t
     let function_param_not_supported = || Err(OutputError::UnsupportedCode(pos.clone(), "Passing functions as parameters is not supported".to_owned())) as Result<(), OutputError>;
     let jump_label_param_illegal = || CompileError::new(pos, format!("JumpLabel not passable as parameter"), ErrorType::TypeError).throw();
     let test_type_illegal = || panic!("TestType");
+    let nested_views_illegal = || CompileError::new(pos, format!("Nested views are illegal"), ErrorType::TypeError).throw();
     match &param_type {
         Type::Array(_, _) => Err(OutputError::UnsupportedCode(pos.clone(), "Passing arrays by value is not supported".to_owned())),
         Type::Function(_, _) => function_param_not_supported(),
         Type::JumpLabel => jump_label_param_illegal(),
         Type::TestType => test_type_illegal(),
-        Type::Primitive(_) => {
-            variable.write_base(out).map_err(OutputError::from)
-        },
+        Type::Primitive(_) => variable.write_base(out).map_err(OutputError::from),
         Type::View(ty) => match &**ty {
-            Type::View(_) => CompileError::new(pos, format!("Nested views are illegal"), ErrorType::TypeError).throw(),
+            Type::View(_) => nested_views_illegal(),
             Type::JumpLabel => jump_label_param_illegal(),
             Type::Function(_, _) => function_param_not_supported(),
             Type::TestType => test_type_illegal(),
             Type::Array(PrimitiveType::Int, dim) => {
                 variable.write_base(out)?;
-                for d in 0..*dim {
-                    variable.write_dim(d, out)?;
-                }
+                write!(out, ", ")?;
+                out.write_comma_separated((0..*dim).map(|d| move |out: &mut CodeWriter| variable.write_dim(d, out)))?;
                 Ok(())
             },
             Type::Primitive(_) => {
                 write!(out, "&")?;
-                variable.write_base(out).map_err(OutputError::from)
+                variable.write_base(out).map_err(OutputError::from)?;
+                Ok(())
             },
         }
     }
@@ -199,10 +215,15 @@ impl CudaWritableExpression for Expression {
             },
             expr => {
                 check_complex_expression_as_param(expr.pos(), param_type)?;
-                write_value_context(expr, out, i32::MIN)
+                self.write_value_context(out)
             }
         }
     }
+
+    fn write_value_context(&self, out: &mut CodeWriter) -> Result<(), OutputError> {
+        write_value_context(self, out, i32::MIN)
+    }
+    
 }
 
 #[cfg(test)]
@@ -221,11 +242,21 @@ fn test_write_value_context_no_unnecessary_brackets() {
 }
 
 #[test]
-fn test_write_base_value_function_call() {
+fn test_write_value_context_function_call() {
     let expr = Expression::parse(&mut fragment_lex("foo[a(b,), c,]")).unwrap();
     let mut output = "".to_owned();
     let mut target = StringWriter::new(&mut output);
     let mut writer = CodeWriter::new(&mut target);
-    write_value_context(&expr, &mut writer, i32::MIN).unwrap();
+    expr.write_value_context(&mut writer).unwrap();
     assert_eq!("_foo_[d1_foo_ * _a_(_b_) + _c_]", output);
+}
+
+#[test]
+fn test_write_value_parameter_context_array() {
+    let expr = Expression::parse(&mut fragment_lex("a")).unwrap();
+    let mut output = "".to_owned();
+    let mut target = StringWriter::new(&mut output);
+    let mut writer = CodeWriter::new(&mut target);
+    expr.write_parameter_context(&Type::View(Box::new(Type::Array(PrimitiveType::Int, 2))), &mut writer).unwrap();
+    assert_eq!("_a_, d0_a_, d1_a_", output);
 }
